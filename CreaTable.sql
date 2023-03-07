ff01e97b52d8ed8e02f44e010bc2a12b327f78f0
biolor <109600227+biolor@users.noreply.github.com>
2023-03-07 13:44:01 +0100

    Update CreaTable.sql

--- CreaTable.sql
+++ CreaTable.sql
@@ -25,3 +25,35 @@ CREATE TABLE public.produit (
 	"Modele-3d" bytea NULL,
 	CONSTRAINT produit_pk PRIMARY KEY (id)
 );
+
+CREATE TABLE public.client (
+	id varchar NOT NULL,
+	nom varchar NULL,
+	prenom varchar NULL,
+	mail varchar NULL,
+	pseudo varchar NULL,
+	telephone varchar NULL,
+	CONSTRAINT client_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE public.entreprise (
+	id varchar NOT NULL,
+	nom varchar NULL,
+	mail varchar NULL,
+	téléphone varchar NULL,
+	CONSTRAINT entreprise_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE public.infos_commande (
+	num_commande int4 NOT NULL,
+	num_produit int4 NULL,
+	quantité int4 NULL,
+	CONSTRAINT infos_commande_pk PRIMARY KEY (num_commande)
+);
+
+CREATE TABLE public.catalogue_revendeur (
+	id_revendeur varchar NOT NULL,
+	id_produit varchar NOT NULL,
+	CONSTRAINT catalogue_revendeur_fk FOREIGN KEY (id_revendeur) REFERENCES public.revendeur(id),
+	CONSTRAINT catalogue_revendeur_fk_1 FOREIGN KEY (id_produit) REFERENCES public.produit(id)
+);
