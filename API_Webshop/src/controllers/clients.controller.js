f9d4c8800f14dd3eec11f96c0f1c45a3685d9595
babaygo <simon.laurent@epsi.fr>
2023-03-07 15:26:19 +0100

    API Webshop => Insertion nouveau client

--- API_Webshop/src/controllers/clients.controller.js
+++ API_Webshop/src/controllers/clients.controller.js
@@ -1,26 +1,27 @@
-const { response } = require('express');
-const { Pool } = require('pg');
+const { Pool, Query } = require('pg');
 
 const pool = new Pool({
-    host: 'belhbwa8yls2jtyfjluy-postgresql.services.clever-cloud.com',
+    host: 'bgjtis2ivq0a1fe0ahiy-postgresql.services.clever-cloud.com',
     user: 'ubmbdses5wpeioit8co1',
     password: 'W8uDI19fylWgm1lDjQrDjLhKYhO83U',
-    database: 'belhbwa8yls2jtyfjluy',
-    port: '5432'
+    database: 'bgjtis2ivq0a1fe0ahiy',
+    port: '5035'
 })
-
-const getTest =  (req, res) => {
-    res.send('Hello');
-}
 
 const setClients = async (req, res) => {
     const { id, nom, prenom, mail, pseudo, telephone } = req.body;
-    await pool.query('INSERT INTO client VALUES ($1, $2, $3, $4, $5, $6)', [id, nom, prenom, mail, pseudo, telephone]);
-    res.send('client cree');
+    await pool.query('INSERT INTO client VALUES ($1, $2, $3, $4, $5, $6)', [id, nom, prenom, mail, pseudo, telephone]).then(response => {
+        if (response) {
+            res.send('client ' + id + ' cree');
+        } else {
+            res.send(response);
+        }
+    }).catch(error => {
+        res.send('Erreur : ' + error);
+    });
+
 }
 
-
 module.exports = {
-    getTest,
     setClients
 }
