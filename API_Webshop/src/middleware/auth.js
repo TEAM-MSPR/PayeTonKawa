273caf90ecb0748663f7e9558b8716ff5d953ecd
babaygo <simon.laurent@epsi.fr>
2023-03-09 12:02:50 +0100

    génération + vérif token

--- API_Webshop/src/middleware/auth.js
+++ API_Webshop/src/middleware/auth.js
@@ -2,8 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        console.log(req);
-        const token = req.headers.authorization;
+        const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
         const pseudo = decodedToken.pseudo;
         req.auth = {
