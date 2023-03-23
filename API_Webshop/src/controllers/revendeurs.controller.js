d30e296fdd4e189b847bc737a5a248dc65a32141
Christopher PUAUD <christopher.puaud@vinci-energies.net>
2023-03-23 10:56:13 +0100

    Rajout d'un renvoyes QRcode

--- API_Webshop/src/controllers/revendeurs.controller.js
+++ API_Webshop/src/controllers/revendeurs.controller.js
@@ -63,13 +63,61 @@ const setRevendeur = async (req, res) => {
 const verifyRevendeur = async (req, res) => {
     const { token, nom } = req.body;
     await pool.query('SELECT COUNT (*) from revendeur WHERE token = $1 AND nom = $2', [token, nom]).then(response => {
-        if(response.rowCount > 0 ){
+        if(response.rows[0].count > 0 ){
             res.send('token verify');
         }
     });
 }
 
+const ReSendQR = async (req, res) => {
+    const { pseudo } = req.body;
+    await pool.query('SELECT mail,token from revendeur WHERE pseudo = $1', [pseudo]).then(response => {
+        if(response){
+           
+            const mail  = response.rows[0].mail;
+            const token  = response.rows[0].token;
+            const data = token;
+
+            qr.toFile('./src/qrcodes/qr-code.png',
+                data, {
+                color: {
+                    dark: '#000',  // Couleur des modules foncés
+                    light: '#fff' // Couleur des modules clairs
+                }
+            }, function (err) {
+                if (err) throw err;
+                console.log('QR code généré !');
+            });
+
+           const attachment = fs.createReadStream('./src/qrcodes/qr-code.png');
+
+            const mailOptions = {
+                from: 'example@' + DOMAIN,
+                to: mail,
+                subject: 'QR Code',
+                text: 'Voici votre QR code en pièce jointe',
+                attachment: attachment
+            };
+
+            mailgun.messages().send(mailOptions, (error, body) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('QR code envoyé par e-mail');
+                }
+            });
+
+            res.send('MAIL renvoyer')
+
+        } else {
+            res.send(response);
+        }
+    });
+}
+
+
 module.exports = {
     setRevendeur,
-    verifyRevendeur
+    verifyRevendeur,
+    ReSendQR
 }
--- API_Webshop/src/routes/router.js
+++ API_Webshop/src/routes/router.js
@@ -2,8 +2,9 @@ const { Router } = require('express');
 const router = Router();
 
 const auth = require('../middleware/auth');
-const { setRevendeur, verifyRevendeur } = require('../controllers/revendeurs.controller');
+const { setRevendeur, verifyRevendeur ,ReSendQR} = require('../controllers/revendeurs.controller');
 
 router.post('/revendeurs', auth, setRevendeur);
+router.post('/revendeurs/ReSendQR', ReSendQR);
 router.get('/revendeurs', verifyRevendeur);
 module.exports = router;
